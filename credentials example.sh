8bab4fa53a6a6ecd552c20a30000ea74727c9f3f
Vadym Pavlenko <pavvadymq@gmail.com>
2024-05-17 19:53:40 +0300

    2 VMs are created and connected in Vagrant
    Created 2 VMs, connected them to each other, provided necessary provision scripts, you need to edit and create credentials.sh files by yourself to make it work

---
+++ credentials example.sh
@@ -0,0 +1,5 @@
+
+DB_NAME="example"
+DB_USER="test"
+DB_PASS="test"
+MAIN_IP="192.168.50.0/24"
---
+++ startup_script.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt-get install postgresql-client --yes
+# install Maven & jdk8
+
+sudo apt-get install openjdk-8-jdk -y
+wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
+tar xzvf apache-maven-3.9.6-bin.tar.gz
+sudo rm -rf apache-maven-3.9.6-bin.tar.gz
+sudo mv apache-maven-3.9.6 /opt/
+
+cat <<EOF >> ~/.bashrc
+
+# Maven environment variables
+
+export M2_HOME=/opt/apache-maven-3.9.6
+export PATH=\${M2_HOME}/bin:\${PATH}
+EOF
+
+source ~/.bashrc
+
+# Install Node 16
+
+curl -s https://deb.nodesource.com/setup_14.x | sudo bash
+sudo apt install nodejs -y
+
+# TomCat install
+
+sudo mkdir /opt/tomcat/
+sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+
+curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89.tar.gz
+sudo tar xzvf apache-tomcat-9.0.89.tar.gz -C /opt/tomcat/ --strip-component=1
+rm -rf apache-tomcat-9.0.89.tar.gz
+
+sudo chown -RH tomcat: /opt/tomcat/
+sudo sh -c 'chmod +x /opt/tomcat/bin/*.sh'
+
+sudo tee /etc/systemd/system/tomcat.service > /dev/null <<EOF
+[Unit]
+Description=Apache Tomcat
+After=network.target
+
+[Service]
+Type=forking
+User=tomcat
+Group=tomcat
+Environment="JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64"
+Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom -Djava.awt.headless=true"
+Environment="CATALINA_BASE=/opt/tomcat"
+Environment="CATALINA_HOME=/opt/tomcat"
+Environment="CATALINA_PID=/opt/tomcat/temp/tomcat.pid"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/opt/tomcat/bin/shutdown.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl start tomcat
+
+
+#####################################
+
+# git clone --branch jira-45-troubleshoot-maven-config--configure-application-and-build-artifact- https://github.com/devops-cisco2024/geocity
+cd ~/geocity
+source /home/vagrant/creds/credentials.sh
+
+# Back-end
+mvn clean install
+sudo mv target/citizen.war /opt/tomcat/webapps/
+
+sudo systemctl restart tomcat
+
+# Front-end
+cd ~/geocity/front-end
+
+npm install
+npm run dev
